498ce2130b3e979ad5c553f7d5307be71b3acfe7
ayush bhatta <bhattaayush107@gmail.com>
2026-04-01 09:44:07 +0530

    Fix Cloudinary TypeScript types

--- backend/src/controllers/foodcontroller.ts
+++ backend/src/controllers/foodcontroller.ts
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import type{ Request,Response } from "express";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!
 });
 
 export const addFood = async (req:Request,res:Response)=>{
